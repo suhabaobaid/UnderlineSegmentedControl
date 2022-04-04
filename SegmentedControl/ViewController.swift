4377745874acc14451e691580cff234f9d04018f
Suha <serenab275@gmail.com>
2022-04-04 11:19:25 -0400

    Initial Commit

---
+++ SegmentedControl/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SegmentedControl
+//
+//  Created by Suha Baobaid on 4/4/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
